4ccfa426ed40845b130b77b6a8285d0787f3641b
Nicolás Raffo Ventura <nraffoventura@gmail.com>
2023-01-24 20:01:46 -0300

    prueba array 1

--- js/script.js
+++ js/script.js
@@ -1,52 +1,27 @@
-alert("Bienvenido a DigitalMarket. Seleccione los productos que desea llevar.")
-let producto = Number(prompt("1 - Celular Gama Media $45.000 / 2 - Celular Gama Alta $140.000 / 3 - Notebook I3 $120.000 / 4 - Notebook I5 $178.000 / 5 - Notebook I7 $215.000 / 6 - TV 32 $80.000 / 7 - TV 42´ $125.000 / 8 - TV 58´ $264.000 / 9 - Reloj digital $55.000 / 0 - Para dejar el carrito"))
-let cantidadProducto;
-let total = 0;
+const productos = [
+    {id: 1, nombre: "Celular Gama Media", precio: 45000},
+    {id: 2, nombre: "Celular Gama Alta", precio: 140000},
+    {id: 3, nombre: "Notebook I3", precio: 120000},
+    {id: 4, nombre: "Notebook I5", precio: 178000},
+    {id: 5, nombre: "Notebook I7", precio: 215000},
+    {id: 6, nombre: "TV 32", precio: 80000},
+    {id: 7, nombre: "TV 42", precio: 125000},
+    {id: 8, nombre: "TV 58", precio: 264000},
+    {id: 9, nombre: "Reloj digital", precio: 55000}
+];
+let carrito = [];
 
-let cantidad = (cantidad, precio) => {
-    return cantidad*precio
+let compra = Number(prompt("Bienvenido a DigitalMarket. Si desea comprar en nuestra tienda ingrese 1, en caso contrario ingrese 0."))
+
+while(compra != 1 && compra != 0){
+    alert("Ese codigo no es valido. Ingrese 1 para ir a nuestra tienda o 0 para salir.")
+    compra = Number(prompt("Bienvenido a DigitalMarket. Si desea comprar en nuestra tienda ingrese 1, en caso contrario ingrese 0."))
 }
 
-while (producto != 0) {
-    switch (producto) {
-        case 1:
-            cantidadProducto = Number(prompt("Indique la cantidad de Celulares Gama Media que va a necesitar: "))
-                total += cantidad(cantidadProducto, 45000)
-            break;
-        case 2:
-            cantidadPorducto = Number(prompt("Indique la cantidad de Celulares Gama Alta que va a necesitar: "))
-                total += cantidad(cantidadProducto, 140000)
-            break;
-        case 3:
-            cantidadPorducto = Number(prompt("Indique la cantidad de Notebook I3 que va a necesitar: "))
-                total += cantidad(cantidadProducto, 120000)
-        break;
-        case 4:
-            cantidadPorducto = Number(prompt("Indique la cantidad de Notebook I5 que va a necesitar: "))
-                total += cantidad(cantidadProducto, 178000)
-            break;
-        case 5:
-            cantidadPorducto = Number(prompt("Indique la cantidad de Notebook I7 que va a necesitar: "))
-                total += cantidad(cantidadProducto, 215000)
-            break;
-        case 6:
-            cantidadPorducto = Number(prompt("Indique la cantidad de TV 32 que va a necesitar: "))
-                total += cantidad(cantidadProducto, 80000)
-            break;
-        case 7:
-            cantidadPorducto = Number(prompt("Indique la cantidad de TV 42 que va a necesitar: "))
-                total += cantidad(cantidadProducto, 125000)
-            break;
-        case 8:
-            cantidadPorducto = Number(prompt("Indique la cantidad de TV 58 que va a necesitar: "))
-                total += cantidad(cantidadProducto, 264000)
-            break;
-        case 9:
-            cantidadPorducto = Number(prompt("Indique la cantidad de Relojes digital que va a necesitar: "))
-                total += cantidad(cantidadProducto, 55000)
-            break;
-    }
-    producto = Number(prompt("1 - Celular Gama Media $45.000 / 2 - Celular Gama Alta $140.000 / 3 - Notebook I3 $120.000 / 4 - Notebook I5 $178.000 / 5 - Notebook I7 $215.000 / 6 - TV 32 $80.000 / 7 - TV 42´ $125.000 / 8 - TV 58´ $264.000 / 9 - Reloj digital $55.000 / 0 - Para dejar el carrito"))
+if(compra == 1){
+    alert("Nuestros productos disponibles son los siguientes:")
+    let listaPorductos = productos.map((producto) => producto.id + " " + producto.nombre + " " + "$" + producto.precio);
+    alert(listaPorductos.join(" - "))
+} else if( compra == 0){
+    alert("Es una pena, pero esperamos que la proxima puedas disfrutrar de nuestros productos. Gracias")
 }
-
-alert("El total de tu compra es: " + total)
